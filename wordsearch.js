17add2c27f064a3f3112c5c098401a4e7723add0
DinoPranjic <dinopranjic92@gmail.com>
2022-02-25 02:09:16 +0000

    implemented wordsearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const horizontalSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        }
     }
-}
+    return false;
+};
 
-module.exports = wordSearch
+const verticalSearch = (allWords, word) => {
+    let index = 0;
+    const words = [];
+    for (let i = 0; i < allWords[0].length; i++) {
+        let newWord = '';
+        allWords.forEach((row, wordsIndex) => {
+            newWord += row[index];
+            if (wordsIndex === allWords.length - 1) {
+                index++;
+            }
+        });
+        words.push(newWord);
+    }
+
+    if (words.includes(word)) {
+        return true;
+    } else if (words.filter((w) => w.includes(word)).length) {
+        return true;
+    }
+
+    return false;
+};
+
+const wordSearch = (letters, word) => {
+    if (!letters.length) {
+        return false;
+    }
+
+    return horizontalSearch(letters, word) || verticalSearch(letters, word);
+};
+
+module.exports = wordSearch;
